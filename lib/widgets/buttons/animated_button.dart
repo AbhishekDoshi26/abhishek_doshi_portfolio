679b4f9f23409b4d2f6f14c5f5bd3735f1f2a6ce
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2023-07-22 21:42:05 +0530

    fix: fixed button tap for mobile

--- lib/widgets/buttons/animated_button.dart
+++ lib/widgets/buttons/animated_button.dart
@@ -1,5 +1,7 @@
+import 'package:abhishek_doshi_portfolio/utils/adaptive.dart';
 import 'package:abhishek_doshi_portfolio/values/values.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class AnimatedButton extends StatefulWidget {
   final String title;
@@ -60,79 +62,100 @@ class _AnimatedButtonState extends State<AnimatedButton> {
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
-    return InkWell(
-      onTap: _isHovering ? widget.onTap : null,
-      hoverColor: Colors.transparent,
-      child: SizedBox(
-        width: widget.width,
-        child: Row(
-          children: [
-            Stack(
-              children: [
-                AnimatedContainer(
-                  width: _isHovering ? widget.width : 0,
-                  height: widget.height,
-                  duration: const Duration(milliseconds: 300),
-                  curve: widget.curve,
-                  decoration: BoxDecoration(
-                    borderRadius: _isHovering
-                        ? widget.trailingButtonHoverBorderRadius
-                        : widget.trailingButtonBorderRadius,
-                    color: widget.trailingButtonColor,
+    if (widthOfScreen(context) < const RefinedBreakpoints().desktopSmall) {
+      return InkWell(
+        onTap: widget.onTap,
+        child: Container(
+          height: widget.height,
+          decoration: BoxDecoration(
+            borderRadius: widget.trailingButtonHoverBorderRadius,
+            color: widget.trailingButtonColor,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              widget.iconData,
+              color: widget.iconColor,
+              size: widget.iconSize,
+            ),
+          ),
+        ),
+      );
+    } else {
+      return InkWell(
+        onTap: _isHovering ? widget.onTap : null,
+        hoverColor: Colors.transparent,
+        child: SizedBox(
+          width: widget.width,
+          child: Row(
+            children: [
+              Stack(
+                children: [
+                  AnimatedContainer(
+                    width: _isHovering ? widget.width : 0,
+                    height: widget.height,
+                    duration: const Duration(milliseconds: 300),
+                    curve: widget.curve,
+                    decoration: BoxDecoration(
+                      borderRadius: _isHovering
+                          ? widget.trailingButtonHoverBorderRadius
+                          : widget.trailingButtonBorderRadius,
+                      color: widget.trailingButtonColor,
+                    ),
                   ),
-                ),
-                MouseRegion(
-                  onEnter: _isHovering ? (e) => _onHover(true) : null,
-                  onExit: _isHovering ? (e) => _onHover(false) : null,
-                  child: SizedBox(
-                    width: widget.width,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                        left: widget.leadingButtonWidth,
-                        top: widget.height / 4,
-                      ),
-                      child: Center(
-                        child: Text(
-                          widget.title,
-                          style: widget.titleStyle ??
-                              textTheme.labelLarge?.copyWith(
-                                color: _isHovering
-                                    ? widget.titleColor
-                                    : Colors.transparent,
-                                fontSize: Sizes.TEXT_SIZE_14,
-                              ),
+                  MouseRegion(
+                    onEnter: _isHovering ? (e) => _onHover(true) : null,
+                    onExit: _isHovering ? (e) => _onHover(false) : null,
+                    child: SizedBox(
+                      width: widget.width,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: widget.leadingButtonWidth,
+                          top: widget.height / 4,
+                        ),
+                        child: Center(
+                          child: Text(
+                            widget.title,
+                            style: widget.titleStyle ??
+                                textTheme.labelLarge?.copyWith(
+                                  color: _isHovering
+                                      ? widget.titleColor
+                                      : Colors.transparent,
+                                  fontSize: Sizes.TEXT_SIZE_14,
+                                ),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                MouseRegion(
-                  onEnter: (e) => _onHover(true),
-                  onExit: (e) => _onHover(false),
-                  child: Container(
-                    width: widget.leadingButtonWidth,
-                    height: widget.height,
-                    decoration: BoxDecoration(
-                      borderRadius: _isHovering
-                          ? widget.leadingButtonHoverBorderRadius
-                          : widget.leadingButtonBorderRadius,
-                      color: widget.leadingButtonColor,
-                    ),
-                    child: Center(
-                      child: Icon(
-                        widget.iconData,
-                        size: widget.iconSize,
-                        color: widget.iconColor,
+                  MouseRegion(
+                    onEnter: (e) => _onHover(true),
+                    onExit: (e) => _onHover(false),
+                    child: Container(
+                      width: widget.leadingButtonWidth,
+                      height: widget.height,
+                      decoration: BoxDecoration(
+                        borderRadius: _isHovering
+                            ? widget.leadingButtonHoverBorderRadius
+                            : widget.leadingButtonBorderRadius,
+                        color: widget.leadingButtonColor,
+                      ),
+                      child: Center(
+                        child: Icon(
+                          widget.iconData,
+                          size: widget.iconSize,
+                          color: widget.iconColor,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 
   void _onHover(bool hovering) {
